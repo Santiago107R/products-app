3b9816a31c2ea4afd55c5459c56fd688d45ecd7b
Ssanti <santiagorobles107k@gmail.com>
2026-02-27 15:51:56 -0300

    second commit

--- app/(products-app)/(home)/index.tsx
+++ app/(products-app)/(home)/index.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 import { ThemedText } from '@/presentation/theme/components/themed-text'
+import React from 'react'
+import { View } from 'react-native'
 
 const HomeScreen = () => {
     return (
-        <View>
-            <ThemedText>HomeScreen</ThemedText>
+        <View className='pt-20 px-10'>
+            <ThemedText className='font-Kanit-Bold text-light-text text-2xl'>HomeScreen</ThemedText>
+            <ThemedText className='font-Kanit-Regular text-2xl'>HomeScreen</ThemedText>
+            <ThemedText className='font-Kanit-Thin text-2xl'>HomeScreen</ThemedText>
         </View>
     )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,49 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/presentation/theme/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import '../global.css'
+
+SplashScreen.preventAutoHideAsync();
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  const [fontsLoaded, error] = useFonts({
+    'KanitBold': require('@/assets/fonts/Kanit-Bold.ttf'),
+    'KanitRegular': require('@/assets/fonts/Kanit-Regular.ttf'),
+    'KanitThin': require('@/assets/fonts/Kanit-Thin.ttf'),
+  })
+// 
+  useEffect(() => { 
+    if (error) throw error
+
+    if (fontsLoaded) SplashScreen.hideAsync()
+
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded && !error) return <ActivityIndicator size={40} />
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        {/* <Stack.Screen
-          name="home"
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen
+          name="(products-app)/(home)/index"
           options={{
             presentation: 'modal',
             title: 'Home Screen'
-          }} /> */}
+          }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
